9154b1e220f24e4c31e65351370bd3c7b5a0062c
Yann GUILLOUX <yann.guilloux@gmail.com>
2011-07-18 21:32:00 +0200

    removed Rails default index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  get "pages/help"
 
-  # The priority is based upon order of creation:
+ match '/contact', :to => "pages#contact"
+ match '/help',    :to => "pages#help"
+ match '/about',   :to => "pages#about"
+
+
+ root :to => "pages#home"
+ # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
