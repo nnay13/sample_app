226a54fee7adb5957e0031e2c35a0d1e2e6c51e0
Yann GUILLOUX <yann.guilloux@gmail.com>
2011-07-27 10:41:03 +0200

    Add gravatar to user show view

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -13,9 +13,18 @@ describe UsersController do
       get :show, :id => @user
       response.should be_success
     end
+
     it "should return the right user" do
       get :show, :id => @user
       assigns[:user] == @user
+    end
+    it "should have the right title" do
+      get :show, :id => @user 
+      response.should have_selector('title',:content => @user.name)
+    end
+    it "should have a profile image" do
+      get :show, :id => @user
+      response.should have_selector('h1>img', :class => "gravatar")
     end
   end
 
